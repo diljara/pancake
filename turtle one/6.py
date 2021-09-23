47d9fab93fc8fe057742b33981a98f1062b7c221
diljara <91018552+diljara@users.noreply.github.com>
2021-09-23 10:33:27 +0300

    Add files via upload

---
+++ turtle one/6.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.shape('turtle')
+for i in range (0,12):
+    turtle.forward(100)
+    turtle.stamp()
+    turtle.backward(100)
+    turtle.left(30)
+
+input()
---
+++ turtle one/8.py
@@ -0,0 +1,11 @@
+import turtle
+import math
+
+turtle.shape('turtle')
+i=10
+while True:
+    turtle.forward(i)
+    turtle.left(90)
+    i=i+10
+
+input()
